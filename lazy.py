6db8a2d0e6a80adadd70abbf32c56c91fd5f0e2f
felixng <felixngb@outlook.com>
2023-11-17 06:42:34 -0800

    use best lazy predictor for classification (SVC)

--- lazy.py
+++ lazy.py
@@ -1,6 +1,12 @@
 from lazypredict.Supervised import LazyClassifier, LazyRegressor
+import lazypredict
 import pandas as pd
 from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.svm import SVC
+from sklearn.model_selection import KFold
+from joblib import dump, load, parallel_backend
 
 # import data
 data = pd.read_csv('trainingset.csv')
@@ -12,15 +18,76 @@ y_classification = (data['ClaimAmount'] > 0).astype(int)
 
 
 # Split the dataset into training and testing sets (classification)
-X_train_c, X_test_c, y_train_c, y_test_c = train_test_split(x, y_classification, test_size=0.2, random_state=42)
+x_train_c, x_test_c, y_train_c, y_test_c = train_test_split(x, y_classification, test_size=0.2, random_state=42)
 
 # Split the dataset into training and testing sets (regression)
 X_train_r, X_test_r, y_train_r, y_test_r = train_test_split(x, y, test_size=0.2, random_state=42)
 
-clf = LazyClassifier(verbose=0, ignore_warnings=True, custom_metric=None)
-models_c, predictions_c = clf.fit(X_train_c, X_test_c, y_train_c, y_test_c)
-print(models_c)
 
-reg = LazyRegressor(verbose=0, ignore_warnings=True, custom_metric=None)
-models_r, predictions_r = reg.fit(X_train_r, X_test_r, y_train_r, y_test_r)
-print(models_r)
+# Remove the high memory classifiers from the list
+highmem_classifiers = ["LabelSpreading","LabelPropagation","BernoulliNB","KNeighborsClassifier", "ElasticNetClassifier", "GradientBoostingClassifier", "HistGradientBoostingClassifier"]
+classifiers = [c for c in lazypredict.Supervised.CLASSIFIERS if c[0] not in highmem_classifiers]
+
+def do_classifiers():
+    clf = LazyClassifier(classifiers=classifiers,verbose=0, ignore_warnings=True, custom_metric=None)
+    models_c, predictions_c = clf.fit(x_train_c, x_test_c, y_train_c, y_test_c)
+    models_c.to_csv('models_c.csv')
+    predictions_c.to_csv('predictions_c.csv')
+    print(models_c, predictions_c)
+
+def do_regressors():
+    # Remove the high memory regressors from the list
+    highmem_regressors = ["GammaRegressor", "GaussianProcessRegressor", "KernelRidge", "QuantileRegressor"]
+    regressors = [reg for reg in lazypredict.Supervised.REGRESSORS if reg[0] not in highmem_regressors]
+
+    reg = LazyRegressor(regressors=regressors, verbose=1, ignore_warnings=True, custom_metric=None)
+    models_r, predictions_r = reg.fit(X_train_r, X_test_r, y_train_r, y_test_r)
+    print(models_r)
+
+def perform_svc(kernel_type):
+    c = [0.0001, 0.001, 0.01, 0.1, 1, 10, 100, 1000]
+    results = []
+    with parallel_backend('threading'):
+        for penalty in c:
+            model = SVC(kernel=kernel_type, C=penalty)
+            cv = KFold(n_splits=10, shuffle=False)
+            fold_results = []
+            for train_index, test_index in cv.split(x_train_c):
+                x_train, x_test = x_train_c.iloc[train_index], x_train_c.iloc[test_index]
+                y_train, y_test = y_train_c.iloc[train_index], y_train_c.iloc[test_index]
+                print("fitting model")
+                model.fit(x_train, y_train)
+                print("predicting")
+                y_pred = model.predict(x_test)
+                fold_results.append(np.mean(y_pred != y_test))
+            results.append(np.mean(fold_results))
+
+    plt.plot(c, results, label='Misclassification Rate')
+    plt.xscale('log')
+    plt.xlabel('C')
+    plt.ylabel('Percentage of misclassifications')
+    plt.title(f'SVM {kernel_type}: % Misclassified vs C')
+    plt.legend()
+    plt.show()
+
+    min_penalty = c[results.index(min(results))]
+    print("Min penalty:", min_penalty)
+    model = SVC(kernel=kernel_type, C=min_penalty)
+    model.fit(x_train_c, y_train_c)
+    predictions = model.predict(x_test_c)
+    percent_misclass = np.mean(predictions != y_test_c)
+    print("Percent misclassification:", percent_misclass)
+
+    plt.scatter(x_test_c.iloc[:, 0], x_test_c.iloc[:, 1], c=y_test_c, s=50, cmap='autumn')
+    # plot_svc_decision_function(model)
+    # plt.title(f'SVM with {kernel_type} kernel')
+    # plt.legend(ytest.unique())
+    # plt.xlabel('x1')
+    # plt.ylabel('x2')
+    # plt.show()
+    # plt.close()
+
+    # save the model
+    dump(model, f'{kernel_type}_SVC_model.joblib')
+
+perform_svc('linear')
