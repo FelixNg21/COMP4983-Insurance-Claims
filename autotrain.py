22f468ec5e6b7c3eedc2fe033b00584c142d1b07
Felix <felix.jh.ng@gmail.com>
2023-11-17 01:02:34 -0800

    removed tf check for GPU since tf 2.11+ does not support GPU on windows anymore

--- autotrain.py
+++ autotrain.py
@@ -1,7 +1,7 @@
 import autokeras as ak
 import pandas as pd
 import numpy as np
-import tensorflow as tf
+
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import load_model
 
@@ -12,10 +12,9 @@ y_data = data['ClaimAmount']  # Labels
 
 xtrain, xval, ytrain, yval = train_test_split(x_data, y_data, test_size=0.2, random_state=42)
 
-tf.config.list_physical_devices('GPU')
 
 def auto_model():
-    reg = ak.StructuredDataRegressor(overwrite=True)
+    reg = ak.StructuredDataRegressor(overwrite=False)
     reg.fit(x_data, y_data, use_multiprocessing=True)
 
     predicted_y = reg.predict(xval)
@@ -23,7 +22,5 @@ def auto_model():
     print(reg.evaluate(xval, yval))
     model = reg.export_model()
     model.summary()
-
-print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 
 auto_model()
